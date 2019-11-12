65bba9b7e8d70f0f68ab9ffecb6e4b5220e77cc3
MLaurentys <matheus.laurentys@usp.br>
2019-11-12 19:38:20 -0300

    making the doodlejump project

---
+++ DoodleJump3D/main.cpp
@@ -0,0 +1,8 @@
+// Include standard headers
+#include "main.h"
+using namespace Simplex;
+
+int main(void)
+{
+    return 0;
+}
---
+++ DoodleJump3D/main.h
@@ -0,0 +1,21 @@
+/*----------------------------------------------
+Programmer: Alberto Bobadilla (labigm@gmail.com)
+Date: 2017/05
+----------------------------------------------*/
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#include "Simplex\Simplex.h"
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
+#include "SFML\OpenGL.hpp"
+#include "SFML\Audio.hpp"
+#include "imgui\ImGuiObject.h"
+
+#endif //__MAIN_H_
+
+/*
+USAGE:
+ARGUMENTS: ---
+OUTPUT: ---
+*/
